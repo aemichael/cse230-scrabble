bf7e1efa2763f704cb537eb3d5bbe4abb6a132ba
Imran Matin <imatin@apple.com>
2021-12-03 15:39:36 -0800

    Implemented functionality to update rack once tile is placed.

--- src/Model.hs
+++ src/Model.hs
@@ -46,6 +46,6 @@ isCurr s r c = Board.pRow p == r && Board.pCol p == c
     p = scrabblePos s 
 
 -- Determines what the next GameState should be based on the current boardState
-next :: Scrabble -> Board.Result Board.Board -> Either (Board.Result ()) Scrabble
-next s Board.Retry     = Right s
-next s (Board.Cont b') = Right (s { scrabbleBoard = b'})
+next :: Scrabble -> Board.Result Board.Board -> Player.Player -> Either (Board.Result ()) Scrabble
+next s Board.Retry     _  = Right s
+next s (Board.Cont b') p'  = Right (s { scrabbleBoard = b', scrabblePlayer = p'})
