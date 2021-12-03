15ef1da10982ef2d36dc6372443497993be520ae
Imran Matin <imatin@apple.com>
2021-12-02 21:11:44 -0800

    Completed commenting.

--- src/View.hs
+++ src/View.hs
@@ -1,3 +1,6 @@
+-------------------------------------------------------------------------------
+-- This module defines how the view for the application is rendered.
+-------------------------------------------------------------------------------
 module View (view) where
 
 import Brick
@@ -12,25 +15,38 @@ import Model.Board
 import Model.Tile
 import Text.Printf (printf)
 
--------------------------------------------------------------------------------
+-- Wrapper function for returning a list of widgets
 view :: GameState -> [Widget String]
--------------------------------------------------------------------------------
 view s = [view' s]
 
+-- Creates a Widget that represents the game board
 view' :: GameState -> Widget String
 view' s = 
   withBorderStyle unicode $
     borderWithLabel (str (header s)) $
       vTile [ mkRow s row | row <- [1..Model.Board.boardDim] ]
 
+-- Prints the header of the game board
 header :: GameState -> String
 header s = printf "Scrabble row = %d, col = %d" (pRow p) (pCol p)
   where 
     p    = gsPos s
 
+-- Creates a row in the board
 mkRow :: GameState -> Int -> Widget n
 mkRow s row = hTile [ mkCell s row i | i <- [1..Model.Board.boardDim] ]
 
+-- TODO: What is this for?
+vTile :: [Widget n] -> Widget n
+vTile (b:bs) = vBox (b : [hBorder <=> b | b <- bs])
+vTile _      = emptyWidget
+
+-- TODO: What is this for?
+hTile :: [Widget n] -> Widget n
+hTile (b:bs) = hBox (b : [vBorder <+> b | b <- bs])
+hTile _      = emptyWidget
+
+-- TODO: What is this for?
 mkCell :: GameState -> Int -> Int -> Widget n
 mkCell s r c 
   | isCurr s r c = withCursor raw 
@@ -38,29 +54,26 @@ mkCell s r c
   where
     raw = mkCell' s r c
 
+-- TODO: What is this for?
 withCursor :: Widget n -> Widget n
 withCursor = modifyDefAttr (`withStyle` reverseVideo)
 
+-- TODO: What is this for?
 mkCell' :: GameState -> Int -> Int -> Widget n
-mkCell' s r c = center (mkTileLetter xoMb)
+mkCell' s r c = center (mkTile xoMb)
   where 
     xoMb      = get (gsBoard s) (BoardPos r c)
 
-mkTileLetter :: Maybe Tile -> Widget n
-mkTileLetter (Just (Letter char)) = blockLetter char
-mkTileLetter (Just (Blank))  = blockBlank
-mkTileLetter Nothing  = blockBlank
+-- Converts a Tile to a widget
+mkTile :: Maybe Tile -> Widget n
+mkTile (Just (Letter char)) = blockLetter char
+mkTile (Just (Blank))  = blockBlank
+mkTile Nothing  = blockBlank
 
+-- Widget for blank tiles
 blockBlank :: Widget n
 blockBlank = vBox [ str " " ]
 
+-- Widget for a tile with a letter
 blockLetter :: Char -> Widget n
 blockLetter char = vBox [ str [(toUpper char)] ]
-
-vTile :: [Widget n] -> Widget n
-vTile (b:bs) = vBox (b : [hBorder <=> b | b <- bs])
-vTile _      = emptyWidget
-
-hTile :: [Widget n] -> Widget n
-hTile (b:bs) = hBox (b : [vBorder <+> b | b <- bs])
-hTile _      = emptyWidget
