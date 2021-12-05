196c5677b9ad61032bb98e17cd617cc22a4da85c
Imran Matin <imatin@ucsd.edu>
2021-12-04 17:57:26 -0800

    Implemented functionality to pick number of players at beginning, but do nothing with that.

--- src/Main.hs
+++ src/Main.hs
@@ -3,46 +3,15 @@
 -------------------------------------------------------------------------------
 module Main where
 
-import           Brick
-import           Brick.BChan (newBChan, writeBChan)
+import PlayScrabble
+import SetupScrabble
 
-import           Control.Concurrent (threadDelay, forkIO)
-import           Control.Monad (forever)
-import           Controller
-import qualified Graphics.Vty as V
-
-import           Graphics.Vty.Attributes
-import           Model
-import           Model.Player
-import           View
 
 -- Starts the application from the initial Scrabble State
 -- Prints the results once the game terminates
 main :: IO ()
 main = do
-  chan   <- newBChan 10
-  forkIO  $ forever $ do
-    writeBChan chan Tick
-    threadDelay 100000 -- decides how fast your game moves
-  let buildVty = V.mkVty V.defaultConfig
-  initialVty <- buildVty
-  -- This line is the entrypoint for the application
-  res <- customMain initialVty buildVty (Just chan) app (Model.initScrabble)
-  -- Print the results once the game terminates
-  print (plScore (scrabblePlayer res)) 
-
--- Constant that defines the application
-app :: App Scrabble Tick String
-app = App
-  { 
-    -- Converts the current application state to the widgets to display
-    appDraw         = View.drawUI 
-    -- Not Important
-    , appChooseCursor = const . const Nothing
-    -- The function to call once an event occurs and needs to be handled
-    , appHandleEvent  = Controller.control 
-    -- The function to run when the application is started
-    , appStartEvent   = Controller.startup
-    -- Attribute map
-    , appAttrMap      = const (attrMap defAttr [])
-  }
+  playerCount <- setupScrabble
+  putStrLn $ "Player Count: " ++ show playerCount
+  score <- playScrabble
+  putStrLn $ "Your final score: " ++ show score
---
+++ src/PlayScrabble.hs
@@ -0,0 +1,51 @@
+-------------------------------------------------------------------------------
+-- This module defines the Scrabble entrypoint into the application.
+-------------------------------------------------------------------------------
+module PlayScrabble (
+    playScrabble
+)
+
+where
+import           Brick
+import           Brick.BChan (newBChan, writeBChan)
+
+import           Control.Concurrent (threadDelay, forkIO)
+import           Control.Monad (forever)
+import           Controller
+import qualified Graphics.Vty as V
+
+import           Graphics.Vty.Attributes
+import           Model
+import           Model.Player
+import           Model.Score
+import           View
+
+-- Plays Scrabble
+playScrabble :: IO (Score)
+playScrabble = do
+  chan   <- newBChan 10
+  forkIO  $ forever $ do
+    writeBChan chan Tick
+    threadDelay 100000 -- decides how fast your game moves
+  let buildVty = V.mkVty V.defaultConfig
+  initialVty <- buildVty
+  -- This line is the entrypoint for the application
+  res <- customMain initialVty buildVty (Just chan) app (Model.initScrabble)
+  -- Print the results once the game terminates
+  return (plScore (scrabblePlayer res)) 
+
+-- Constant that defines the application
+app :: App Scrabble Tick String
+app = App
+  { 
+    -- Converts the current application state to the widgets to display
+    appDraw         = View.drawUI 
+    -- Not Important
+    , appChooseCursor = const . const Nothing
+    -- The function to call once an event occurs and needs to be handled
+    , appHandleEvent  = Controller.control 
+    -- The function to run when the application is started
+    , appStartEvent   = Controller.startup
+    -- Attribute map
+    , appAttrMap      = const (attrMap defAttr [])
+  }
