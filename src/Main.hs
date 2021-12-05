9284d388596cc529055cf2e5640ae51d4bbb73a5
Imran Matin <imatin@ucsd.edu>
2021-12-04 19:35:12 -0800

    Added initial screen to prompt for player count.

--- src/Main.hs
+++ src/Main.hs
@@ -4,15 +4,15 @@
 module Main where
 
 import PlayScrabble
--- import SetupScrabble
+import SetupScrabble
 
 
 -- Starts the application from the initial Scrabble State
 -- Prints the results once the game terminates
 main :: IO ()
 main = do
-  -- playerCount <- setupScrabble
-  -- putStrLn $ "Player Count: " ++ show playerCount
+  playerCount <- setupScrabble
+  putStrLn $ "Player Count: " ++ show playerCount
   let playerCount = 1
   score <- playScrabble playerCount
   putStrLn $ "Your final score: " ++ show score
